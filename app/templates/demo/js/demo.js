5b2b91c312a7c5376a0870d7f416ab52867dcf69
Taihoon Kim <kishu@navercorp.com>
2017-04-26 18:17:06 +0900

    Revert "chore(template): demo for jekyll"
    This reverts commit 7e9b7b6703b9bfffd1e16184220659dcd73c3114.

---
+++ app/templates/demo/js/demo.js
@@ -0,0 +1,6 @@
+/**
+ * Copyright (c) <%= options.author %>
+ */
+
+var <%= componentName %> = new eg.<%= upperFirst(componentName) %>;
+console.log("Hello eg.<%= upperFirst(componentName) %>!!!", <%= componentName %>);
