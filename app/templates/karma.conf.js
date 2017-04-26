3d6c3129b35a586934a0ea9c6e108cb215020281
Taihoon Kim <kishu@navercorp.com>
2017-04-26 11:14:56 +0900

    chore(template): update karma.conf.js

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -1,16 +1,57 @@
 module.exports = function(config) {
 	config.set({
-		frameworks: ["qunit"],
+		frameworks: ["mocha", "chai"],
+
 		files: [
-			// vendor files
-			"node_modules/jquery/dist/jquery.js",
-			"node_modules/iscroll/build/iscroll.js",
-			// src files
-			"dist/<%= componentName.toLowerCase() %>.js",
-			// test files
-			"test/unit/js/**/*.js"
+			"./node_modules/phantomjs-polyfill/bind-polyfill.js",
+			"./node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js",
+			"./test/unit/*.spec.js"
 		],
-		browsers: ["PhantomJS"],
-		singleRun: true
+
+		preprocessors: {
+			"./test/unit/*.spec.js": ["webpack"]
+		},
+
+		webpack: {
+			devtool: "inline-source-map",
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						loader: "babel-loader",
+						options: {
+							presets: ["es2015"],
+							plugins: ["add-module-exports"]
+						}
+					}
+				]
+			}
+		},
+
+		webpackMiddleware: {
+			noInfo: true
+		},
+
+		reporters: ["mocha"],
+
+		browsers: ["PhantomJS_custom"],
+
+		customLaunchers: {
+			"PhantomJS_custom": {
+				base: "PhantomJS",
+				options: {
+					viewportSize: {
+						width: 1024,
+						height: 960
+					}
+				}
+			}
+		},
+
+		client: {
+			useIframe: false,
+			runInParent: true
+		}
 	});
 };
